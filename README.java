dd8fe6199e522e5b2216d69dd11620bb1f0e857d
prakashpks <prakars878740@gmail.com>
2016-07-08 15:40:04 +0530

    finish

---
+++ README.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+
+public class number {
+
+	
+	public static void main(String[] args) {
+		 int num;
+		    System.out.println("Enter a positive Integer");  
+		    Scanner s = new Scanner(System.in);
+		     num= s.nextInt();
+		    
+		    
+		     
+		     switch(num)
+		     {
+		     case 0:
+		         System.out.println("zero"); 
+		         break;
+		      case 1:
+		         System.out.println("one"); 
+		         break; 
+		      case 2:
+		         System.out.println("two"); 
+		         break; 
+		       case 3:
+		         System.out.println("three"); 
+		         break;   
+		       case 4:
+		         System.out.println("four"); 
+		         break;   
+		       case 5:
+		         System.out.println("five"); 
+		         break;   
+		        case 6:
+		         System.out.println("six"); 
+		         break;
+		       case 7:
+		         System.out.println("seven"); 
+		         break;   
+		       case 8:
+		         System.out.println("eight"); 
+		         break;   
+		         
+		       case 9:
+		         System.out.println("nine"); 
+		         break;  
+		       default:
+		        
+		         System.out.println("please enter number below 10"); 
+		         break;
+		         
+		         }
+		         }
+         
+         
